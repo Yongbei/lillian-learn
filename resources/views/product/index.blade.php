261b285a69a815165bd93a5c87b07c6cfe4744a2
Yong <38073860+Yongbei@users.noreply.github.com>
2023-11-15 17:37:27 +0800

    add table feature

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -23,5 +23,31 @@
             </li>
         @endforeach
     </ul>
+
+    <table style="border: 1px solid black;">
+        <tr>
+            <th style="border: 1px solid black;">玩偶名稱</th>
+            <th style="border: 1px solid black;">修改</th>
+            <th style="border: 1px solid black;">刪除</th>
+        </tr>
+        @foreach ($products as $product)
+            <tr>
+                <td style="border: 1px solid black;">
+                    <a href="/products/{{ $product->id }}">{{ $product->name }}</a>
+                </td>
+                <td style="border: 1px solid black;">
+                    <a href="/products/{{ $product->id }}/edit">修改</a>
+                </td>
+                <td style="border: 1px solid black;">
+                    <form action="/products/{{ $product->id }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <input type="submit" value="刪除">
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
 </body>
 </html>
